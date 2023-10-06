02ce043f85cc5091878e85767af74a67214d3848
Mariia Akymenko <dafnarianna@gmail.com>
2023-10-06 22:01:48 +0300

    create app

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// const contactsRouter = require('./routes/api/contacts');
+// const usersRouter = require('./routes/api/users');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+// app.use('/api/contacts', contactsRouter);
+// app.use('/api/users', usersRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found' });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = 'Server error' } = err;
+  res.status(status).json({ message });
+});
+
+export default app;
