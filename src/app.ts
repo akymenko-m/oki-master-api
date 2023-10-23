0fa29a86593a7bcd307c766ed57d21f5c7cb2e06
Mariia Akymenko <dafnarianna@gmail.com>
2023-10-24 00:43:40 +0300

    + add controllers for archived orders router

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
-import express from 'express';
 import cors from 'cors';
+import express, { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
 import dotenv from 'dotenv';
-
-import ordersRouter from './routes/api/orders';
+import { archiveRouter, ordersRouter } from 'routes/api';
 
 dotenv.config();
 
@@ -12,15 +11,22 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
+app.use('/api/archive', archiveRouter);
 app.use('/api/orders', ordersRouter);
 
-app.use((req, res) => {
+app.use((req: Request, res: Response) => {
   res.status(404).json({ message: 'Not found' });
 });
 
-app.use((err, req, res, next) => {
+interface CustomError extends Error {
+  status?: number;
+}
+
+const errorHandler: ErrorRequestHandler = (err: CustomError, req: Request, res: Response, next: NextFunction) => {
   const { status = 500, message = 'Server error' } = err;
   res.status(status).json({ message });
-});
+};
+
+app.use(errorHandler);
 
 export default app;
